ff93a2f07b6af305a8e99cbf210fef80d23358b5
Kruasanow <73633964+Kruasanow@users.noreply.github.com>
2023-03-17 12:08:18 +0300

    123

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, sessions, url_for, request, session, redirect
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+DEBUG = True 
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['get','post'])
+def index():
+    print(url_for('index'))
+
+    if request.method == "POST":
+        ident = request.form['choose_rest']
+        
+        return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def error404Catch(error):
+    return render_template('error404.html', Title = 'Страница не найдена'), 404
+#-----LOAD------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
